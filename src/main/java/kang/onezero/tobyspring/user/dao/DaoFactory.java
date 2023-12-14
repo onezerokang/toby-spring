7e1dab8d896bd6fd1dfc7029e415bb2e3f796c19
Wonyoung Kang <onezerokang@gmail.com>
2023-12-14 15:14:03 +0900

    의존 관계를 맺는 Factory

---
+++ src/main/java/kang/onezero/tobyspring/user/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package kang.onezero.tobyspring.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao() {
+        UserDao userDao = new UserDao(connectionMaker());
+        return userDao;
+    }
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+}

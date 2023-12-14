28771574b03a1a3ed992a4e832bb634ebe7a24b9
Wonyoung Kang <onezerokang@gmail.com>
2023-12-14 15:19:34 +0900

    ApplicationContext 적용

--- src/main/java/kang/onezero/tobyspring/user/dao/DaoFactory.java
+++ src/main/java/kang/onezero/tobyspring/user/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package kang.onezero.tobyspring.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- src/main/java/kang/onezero/tobyspring/user/dao/UserDaoTest.java
+++ src/main/java/kang/onezero/tobyspring/user/dao/UserDaoTest.java
@@ -1,12 +1,16 @@
 package kang.onezero.tobyspring.user.dao;
 
 import kang.onezero.tobyspring.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("faker");

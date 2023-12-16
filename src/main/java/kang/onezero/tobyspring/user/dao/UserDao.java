36950e374773ed0b060ef706ec05dd25fb152293
Wonyoung Kang <onezerokang@gmail.com>
2023-12-16 17:02:33 +0900

    ConnectionMaker를 DataSource로 리팩토링

--- src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
+++ src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
@@ -2,25 +2,19 @@ package kang.onezero.tobyspring.user.dao;
 
 import kang.onezero.tobyspring.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    public UserDao() {
-    }
-
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
-    }
-
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -35,7 +29,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");

84673e52d285462b77b6e9c1f2c189b8331580e6
Wonyoung Kang <onezerokang@gmail.com>
2023-12-17 20:08:01 +0900

    학습 테스트

--- src/test/java/kang/onezero/tobyspring/user/dao/UserDaoTest.java
+++ src/test/java/kang/onezero/tobyspring/user/dao/UserDaoTest.java
@@ -1,26 +1,40 @@
 package kang.onezero.tobyspring.user.dao;
 
+import jakarta.annotation.security.RunAs;
 import kang.onezero.tobyspring.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(locations = "/applicationContext.xml")
 class UserDaoTest {
+    @Autowired
+    private ApplicationContext context; // 테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값이 주입된다.
+
+    @Autowired
     private UserDao dao;
+
     private User user1;
     private User user2;
     private User user3;
 
     @BeforeEach
     public void setUp() {
-        GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        this.dao = context.getBean("userDao", UserDao.class);
-
         this.user1 = new User("faker", "이상혁", "1234");
         this.user2 = new User("zeus", "최우제", "5678");
         this.user3 = new User("keria", "류민석", "9012");

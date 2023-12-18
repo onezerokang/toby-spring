d3a9a64c9aa60a08197bf7e608c8fec4718d2e7b
Wonyoung Kang <onezerokang@gmail.com>
2023-12-18 18:50:53 +0900

    템플릿/콜백 패턴 실습 part1

---
+++ src/test/java/kang/onezero/tobyspring/learningtest/template/BufferedReaderCallback.java
@@ -0,0 +1,8 @@
+package kang.onezero.tobyspring.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface BufferedReaderCallback {
+    Integer doSomethingWithReader(BufferedReader br) throws IOException;
+}

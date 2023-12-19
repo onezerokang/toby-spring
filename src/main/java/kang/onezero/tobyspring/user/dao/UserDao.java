2b93b0640109b9faecd468e1fac41ee06e0835a4
Wonyoung Kang <onezerokang@gmail.com>
2023-12-19 18:53:09 +0900

    UserDao 주석 및 SQLException 정리

--- src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
+++ src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
@@ -1,12 +1,7 @@
 package kang.onezero.tobyspring.user.dao;
 
-import com.mysql.cj.result.Row;
 import kang.onezero.tobyspring.user.domain.User;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
@@ -35,39 +30,16 @@ public class UserDao {
         this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
+    public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
                 new Object[]{id}, this.userRowMapper);
     }
 
     public void deleteAll() {
-//        this.jdbcTemplate.update(
-//                new PreparedStatementCreator() {
-//                    @Override
-//                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//                        return con.prepareStatement("delete from users");
-//                    }
-//                }
-//        );
-
-//        위 코드와 같다.
         this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws SQLException {
-//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//            @Override
-//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//                return con.prepareStatement("select count(*) from users");
-//            }
-//        }, new ResultSetExtractor<Integer>() {
-//            @Override
-//            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//                rs.next();
-//                return rs.getInt(1);
-//            }
-//        });
-
+    public int getCount() {
         return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 

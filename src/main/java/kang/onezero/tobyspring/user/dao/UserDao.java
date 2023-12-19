cf39bf6a73a9f3cab32cc21dd15e272b9eb3c6b2
Wonyoung Kang <onezerokang@gmail.com>
2023-12-19 17:57:19 +0900

    JdbcTemplate.queryForObject() 템플릿 메소드와 PreparedStatementCreator, ResultSetExtractor 콜백 오브젝트를 사용하여 UserDao.getCount() 메소드 리팩토링

--- src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
+++ src/main/java/kang/onezero/tobyspring/user/dao/UserDao.java
@@ -1,9 +1,11 @@
 package kang.onezero.tobyspring.user.dao;
 
 import kang.onezero.tobyspring.user.domain.User;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -65,41 +67,20 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//                return con.prepareStatement("select count(*) from users");
+//            }
+//        }, new ResultSetExtractor<Integer>() {
+//            @Override
+//            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//                rs.next();
+//                return rs.getInt(1);
+//            }
+//        });
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
 

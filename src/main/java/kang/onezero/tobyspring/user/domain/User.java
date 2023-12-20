344c9269f0fbeefe0e6318b8c0ce1c897349f925
Wonyoung Kang <onezerokang@gmail.com>
2023-12-20 09:19:09 +0900

    User 필드 추가

--- src/main/java/kang/onezero/tobyspring/user/domain/User.java
+++ src/main/java/kang/onezero/tobyspring/user/domain/User.java
@@ -4,15 +4,21 @@ public class User {
     String id;
     String name;
     String password;
+    Level level;
+    int login;
+    int recommend;
 
     // 자바빈 규약을 따르는 클래스에 생성자를 명시적으로 추가할 때는 디폴트 생성자를 정의해주는 것을 잊지 말자.
     public User() {
     }
 
-    public User(String id, String name, String password) {
+    public User(String id, String name, String password, Level level, int login, int recommend) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.level = level;
+        this.login = login;
+        this.recommend = recommend;
     }
 
     public String getId() {
@@ -37,5 +43,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
     }
 }

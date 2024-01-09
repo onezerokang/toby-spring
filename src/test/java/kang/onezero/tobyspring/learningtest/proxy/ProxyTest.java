c2df7759c4a38200626a9193db6e3aeb2f17380b
Wonyoung Kang <onezerokang@gmail.com>
2024-01-09 16:11:40 +0900

    Dynamic Proxy 적용

--- src/test/java/kang/onezero/tobyspring/learningtest/proxy/ProxyTest.java
+++ src/test/java/kang/onezero/tobyspring/learningtest/proxy/ProxyTest.java
@@ -2,6 +2,8 @@ package kang.onezero.tobyspring.learningtest.proxy;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProxyTest {
@@ -13,6 +15,18 @@ public class ProxyTest {
         assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
 
         Hello proxiedHello = new HelloUppercase(hello);
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void dynamicProxy() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+            getClass().getClassLoader(), // 동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더
+            new Class[] { Hello.class }, // 구현할 인터페이스
+            new UppercaseHandler(new HelloTarget())); // 부가기능과 위임 코드를 담은 InvocationHandler
+
         assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
         assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
         assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");

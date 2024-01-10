284d0804018270974f69a225e35e4ceeb19aa7e0
Wonyoung Kang <onezerokang@gmail.com>
2024-01-10 21:36:50 +0900

    ProxyFactoryBean을 이용해 프록시 만들기

--- src/test/java/kang/onezero/tobyspring/learningtest/proxy/ProxyTest.java
+++ src/test/java/kang/onezero/tobyspring/learningtest/proxy/ProxyTest.java
@@ -1,6 +1,10 @@
 package kang.onezero.tobyspring.learningtest.proxy;
 
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 import java.lang.reflect.Proxy;
 
@@ -30,5 +34,27 @@ public class ProxyTest {
         assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
         assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
         assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget()); // 타깃 설정
+        pfBean.addAdvice(new UppercaseAdvice());; // 부가기능을 담은 어드바이스. 여러 개 추가 가능
+
+        Hello proxiedHello = (Hello) pfBean.getObject(); // FactoryBean이므로 getObject()로 생성된 프록시를 가져온다.
+
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    static class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            // 리플렉션의 Method와 달리 메소드 실행 시 타깃 오브젝트를 전달할 필요가 없다. MethodInvocation은 메소드 정보와 함께 타깃 오브젝트를 알고 있기 때문이다.
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase(); // 부가기능 적용
+        }
     }
 }
